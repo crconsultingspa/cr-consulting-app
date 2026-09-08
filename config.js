46563adb6a4bdc4751ce168dc96db947374dc5b7
crconsultingspa <crconsulting.cl@gmail.com>
2026-09-08 19:58:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,4 @@
 // (Gestionar implementaciones → lápiz → Nueva versión → Implementar), la URL
 // NO cambia y no hace falta editar nada de esto.
 // ─────────────────────────────────────────────────────────────────────────────
-window.SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxNqrOUp_Z-LbmBXj_5pUg2zhI_b8zBP-ppNwjgm7TJI5RP0O2PKejNaDUvV4sK32QxAw/exec';
+window.SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwEbF3jzveJVxglRBPNBOGwp0xf6uQXazYrzi6rUTDCaVmoi4zbIBWNe62t91eqXQk/exec';
